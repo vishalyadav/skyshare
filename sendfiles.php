8cd47df8cddda34481c80a2654f590de215a7e22
vishal <vishal_s_yadav@yahoo.com>
2013-09-07 20:50:37 -0700

    Redirecting page but too tired to function

--- sendfiles.php
+++ sendfiles.php
@@ -1,1 +1,23 @@
-hfwliuwehfiwahfiuewhfaw
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>SkyShare</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Bubbler+One' rel='stylesheet' type='text/css'>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+	<script src="js/script.js"></script>
+</head>
+<body>
+	<div id="main_box">
+	</div>
+	<div id="main_main">
+		<div id="title">
+			SkyShare
+		</div>
+		<?php
+			echo $_POST["code"];
+		?>
+	</div>
+</body>
+</html>
