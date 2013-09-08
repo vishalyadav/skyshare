10086dcc03700bed27b28dd73d12a85d3a27d89d
vishal <vishal_s_yadav@yahoo.com>
2013-09-07 18:27:27 -0700

    Added some id process logic

--- code_validate.php
+++ code_validate.php
@@ -1,6 +1,6 @@
 <?php
-/*	$code = $_POST["code"];
-	$server = 'tuqhxl05rd.database.windows.net,1433';
+	$code = $_POST["code"];
+/*	$server = 'tuqhxl05rd.database.windows.net,1433';
 
 	//$link = mssql_connect($server, "ashish@tuqhxl05rd", "PennApps2013");
 
@@ -19,15 +19,26 @@
 	}*/
 
 	$server = "tuqhxl05rd.database.windows.net,1433";
-$username = "ashish@tuqhxl05rd";
-$password = "PennApps2013";
-$database = "SkyShareDB";
-try
-{
-    $conn = new PDO("sqlsrv:server=$server ; Database = $database", $username, $password);
-}
-catch(Exception $e)
-{
-    die(print_r($e));
-}
+	$username = "ashish@tuqhxl05rd";
+	$password = "PennApps2013";
+	$database = "SkyShareDB";
+	try
+	{
+	    $conn = new PDO("sqlsrv:server=$server ; Database = $database", $username, $password);
+	}
+	catch(Exception $e)
+	{
+	    die(print_r($e));
+	}
+	$sql = "SELECT CodeURL FROM CodeDB WHERE Code=$code";
+	$stmt = $conn->query($sql, PDO::FETCH_COLUMN, 0);
+
+	if($stmt === false)
+	{
+	      die(print_r(sqlsrv_errors(), true));
+	}
+
+	while ( $row = $stmt->fetch() ){
+	   echo "$row\n";
+	}
 ?>
